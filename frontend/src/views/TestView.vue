9b068fb6460f9c90d9d74b734d2ef986b4fda035
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-01-15 16:46:16 -0300

    Importando axios + testes de consumo de front para backend

--- frontend/src/views/TestView.vue
+++ frontend/src/views/TestView.vue
@@ -3,12 +3,28 @@
     <div>
         <title>aaa</title>
         <h1>Teste</h1>
+        <p>{{ testMsg }}</p>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+    data(){
+        return{
+            testMsg: ''
+        }
+    },
 
+    created(){
+        axios.get('http://localhost:2300/test')
+        .then((res) =>{
+            console.log(res)
+            this.testMsg = res.data.msg
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
 }
 </script>
 
