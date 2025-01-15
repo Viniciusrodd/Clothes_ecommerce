7138e9733f442d96072c613e39e91c304fac0209
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-01-15 15:45:16 -0300

    Criando primeiro teste para rota de teste
    Iniciando projeto com metodologia TDD

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -1,1 +1,12 @@
 
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
+
+
+test('Should testing if "/test" its working', () =>{
+    return request.get('/test')
+    .then((res) =>{
+        expect(res.statusCode).toEqual(200);
+    }).catch(error => console.log(error));
+})
