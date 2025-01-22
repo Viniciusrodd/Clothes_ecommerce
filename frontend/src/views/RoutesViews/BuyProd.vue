5cbe6f8df20901faa5d6f833015263ea23a35759
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-01-22 17:29:35 -0300

    Estruturando estilo da view compras

--- frontend/src/views/RoutesViews/BuyProd.vue
+++ frontend/src/views/RoutesViews/BuyProd.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="app">
         <header id="headerhome">
-            <div id="logo"></div>
+            <router-link :to="{name: 'Homepage'}">
+                <div id="logo"></div>
+            </router-link>
             <h1 class="title is-1">El Lojita</h1>
             <div id="icons">
                 <i class="material-icons">mail</i>
@@ -10,18 +12,40 @@
             </div>
         </header>
 
-        <hr class="hr">         
+        <hr class="hr">     
+        
+        <div id="checkout-container">
+            <div id="product-container">
+                <div id="divImagens" ref="imgRef">
+                    <img class="newImg" v-if="product.image == undefined" src="../../styles/images/sem_imagem.png" alt="Imagem do produto" />
+                    <img class="newImg" v-if="product.image" :src="`data:image/png;base64,${product.image}`" alt="Imagem do produto" />
+                </div>
+            </div>
+            <div id="infos-container">
+                <p class="title is-2">{{ product.name }}</p>
+                <p class="title is-3">R$ {{ product.price }}</p>
+        
+                <div class="select is-info">
+                    <select name="size" id="isize">
+                        <option value="" selected>Selecione o tamanho</option>
+                        <option value="P">P</option>
+                        <option value="M">M</option>
+                        <option value="G">G</option>
+                        <option value="GG">GG</option>
+                    </select>
+                </div>
 
-        <div id="divImagens" ref="imgRef">
-            <img class="newImg" v-if="product.image == undefined" src="../../styles/images/sem_imagem.png" alt="Imagem do produto" />
-            <img class="newImg" v-if="product.image" :src="`data:image/png;base64,${product.image}`" alt="Imagem do produto" />
+                <div id="quantity-container">
+                    <input type="number" name="quantity" id="iquantity" class="input is-info" value="1" min="1" max="9" autocomplete="off">
+                    <button id="comprar-btt-perso" class="button is-info is-inverted is-outlined">COMPRAR</button>                    
+                </div>
+
+                <hr class="hr">
+                <h1 class="title is-3">Descrição:</h1>
+                <p>{{ product.description }}</p>
+            </div>
         </div>
-        <p class="title is-3">{{ product.name }}</p>
-        <p class="title is-4">R$ {{ product.price }}</p>
 
-        <button class="button is-link is-outlined">
-            COMPRAR
-        </button>
     </div>
 </template>
 
@@ -60,5 +84,5 @@ export default {
 </script>
 
 <style>
-
+    @import '../../styles/routesStyles/BuyProd.css';
 </style>
