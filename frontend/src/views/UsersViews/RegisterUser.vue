631e808d4723c345420d9bec82567c567208774b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-01-26 15:39:11 -0300

    Estruturando/estilizando view de registro de user

--- frontend/src/views/UsersViews/RegisterUser.vue
+++ frontend/src/views/UsersViews/RegisterUser.vue
@@ -1,21 +1,24 @@
 <template>
-    <div id="app">
-        <HeaderComp/>
+    <div id="appRegister" style="height: 90vh;">
         <h1 class="title is-1">Registro de usuário</h1>
+        <hr class="hr">
+        <form @submit.prevent="submitForm">
+            <input class="input is-hovered" type="text" name="name" id="iname" placeholder="Nome de usuário" autocomplete="off" required>
+            <input class="input is-hovered" type="email" name="email" id="iemail" placeholder="Email de usuário" autocomplete="off" required>
+            <input class="input is-hovered" type="text" name="password" id="ipassword" placeholder="Senha de usuário" autocomplete="off" required>
+
+            <button class="button is-link is-light">Registrar</button>
+        </form>
     </div>
 </template>
 
 <script>
-import HeaderComp from '@/components/HeaderComp.vue';
 export default {
-    components: {
-        HeaderComp
-    },
 
 
 }
 </script>
 
 <style>
-
+    @import '../../styles/usersStyles/registerUser.css';
 </style>
