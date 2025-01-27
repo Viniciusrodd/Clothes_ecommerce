340ffdcc02c25f68d1612a6cc7f2e92c8fbf6658
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-01-27 16:06:22 -0300

    Estruturando + estilizando view de myAccount

--- frontend/src/views/UsersViews/MyAccount.vue
+++ frontend/src/views/UsersViews/MyAccount.vue
@@ -3,7 +3,21 @@
     <div id="app">
         <header-comp/>
         <hr class="hr">
-        
+        <h1 id="h1-myAccount" class="title is-2">Já possui uma conta ?</h1>
+        <p id="subtitle" class="subtitle is-3">Faça seu login</p>
+
+        <form @submit.prevent="submitForm" id="form-myAccount">
+            <input class="input is-hovered" v-model="formData.name" type="text" name="name" id="iname" placeholder="Nome" autocomplete="off" required>
+            <input class="input is-hovered" v-model="formData.email" type="email" name="email" id="iemail" placeholder="Email" autocomplete="off" required>
+            <input class="input is-hovered" v-model="formData.password" type="password" name="password" id="ipassword" placeholder="Senha" autocomplete="off" required>
+
+            <button class="button is-link is-light">Login</button>
+        </form>
+
+        <h1 id="h1-myAccount" class="title is-3">Não possui uma conta ?</h1>
+        <router-link :to="{name: 'Register'}" target="_blank">
+            <p id="p-cadastre" class="subtitle is-3">Cadastre-se aqui</p>
+        </router-link>
     </div>
 </template>
 
@@ -12,6 +26,22 @@ import HeaderComp from '@/components/HeaderComp.vue';
 export default {
     components: {
         HeaderComp
+    },
+
+    data(){
+        return {
+            formData: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    },
+
+    methods: {
+        submitForm(){
+            console.log('ads')
+        }
     }
 }
 </script>
