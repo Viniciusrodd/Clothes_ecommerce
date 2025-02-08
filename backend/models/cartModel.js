cf043dd5f42983cffceb2deabd88c9611a46c0f8
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-02-08 15:26:19 -0300

    Criação de model associativo de cart com mongo

---
+++ backend/models/cartModel.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    //user reference
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user', //ref model de user
+        required: true //required field
+    },
+    //product reference
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'clothes',
+        required: true
+    },
+    //quantity of products
+    quantity: {
+        type: Number,
+        default: 1
+    }
+}, {
+    timestamps: true //Fields createdAt e updatedAt automatically created
+});
+
+//model
+const cartModel = mongoose.model('cart', cartSchema);
+
+module.exports = cartModel;
