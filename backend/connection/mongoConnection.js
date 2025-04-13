9e749b4e5f2dfdf48114c91e0e515392601e2b15
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-13 09:51:14 -0300

    Removendo pastas/arquivos do backend versionados pelo git

--- backend/connection/mongoConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.DB_CONNECT)
-    .then(() =>{
-        console.log('Mongo DB connected');
-    }).catch(error => console.log('Erro at mongoDB connection', error));
-
-module.exports = mongoose;
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 2300;
-
-app.listen(PORT, '0.0.0.0', () =>{
-    console.log(`Server at backend side opens in ${PORT}`);
-});
